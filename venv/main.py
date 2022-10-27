a935d4d70fd2a08e8877e5336a1ea44d0dea31e6
Dodero10 <truongdat.work@gmail.com>
2022-10-27 20:39:46 +0700

    update

--- venv/main.py
+++ venv/main.py
@@ -1,62 +1,34 @@
 #detect face of images
+
 import cv2
 import os
-image_path = '../img/peo10.jpg'
+
+face_detector = cv2.CascadeClassifier('..\haarcascades\haarcascade_frontalface_alt.xml')
+def getFaces(img_path):
+    img = cv2.imread(image_path)
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(img_gray,1.3,5)
+    count = 0
+    for (x, y, w, h) in faces:
+        img_face_cut = cv2.resize(img[y:y + h,x:x + w],(64,64))
+        cv2.imwrite('img/peo1_{}.jpg'.format(count),img_face_cut)
+        count +=1
+
+image_path = '..\img'
+
+for file in os.listdir(image_path):
+    file_path = os.path.join(image_path,file)
+    for sub_file in os.listdir(file_path):
+        img_path = os.path.join(file_path, sub_file)
+        if not os.path.isdir(file_path.replace('img','img_faces')):
+            os.mkdir(file_path.replace('img','img_faces'))
 
 
 
 
+        if sub_file.endswith('.jpg'):
+            getFaces(img_path)
 
 
 
-
-
-
-
-
-
-
-
-# face_detector = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_alt.xml')
-#
-# img = cv2.imread(image_path)
-# img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#
-# while True:
-#     faces = face_detector.detectMultiScale(img_grey,1.3,5)
-#     count = 0
-#
-#     for (x, y, w, h) in faces:
-#         img_face_cut = cv2.resize(img[y + 2:y + h - 2, x + 2:x + w - 2],(64,64))
-#         cv2.imwrite('img/peo1_{}.jpg'.format(count),img_face_cut)
-#         cv2.rectangle(img, (x, y),(x+w,y+h),(0,255,0),2)
-#         count +=1
-#
-#
-#     cv2.imshow('FRAME', img)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cv2.destroyAllWface_detector = cv2.CascadeClassifier('haarcascades\haarcascade_frontalface_alt.xml')
-#
-# img = cv2.imread(image_path)
-# img_grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#
-# while True:
-#     faces = face_detector.detectMultiScale(img_grey,1.3,5)
-#     count = 0
-#
-#     for (x, y, w, h) in faces:
-#         img_face_cut = cv2.resize(img[y + 2:y + h - 2, x + 2:x + w - 2],(64,64))
-#         cv2.imwrite('img/peo1_{}.jpg'.format(count),img_face_cut)
-#         cv2.rectangle(img, (x, y),(x+w,y+h),(0,255,0),2)
-#         count +=1
-#
-#
-#     cv2.imshow('FRAME', img)
-#
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
