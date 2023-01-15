f034d2fcbbd5da401a7c8a00a05116abf9c67a64
Truong Cong Dat <truongdat.work@gmail.com>
2023-01-16 00:21:10 +0700

    Face detection using webcam or video

--- main.py
+++ main.py
@@ -4,36 +4,38 @@ import os
 face_detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt.xml')
 
 
-def getFaces(image_path):
-    img = cv2.imread(image_path)
+# def getFaces(image_path):
+#     img = cv2.imread(image_path)
 
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces = face_detector.detectMultiScale(img_gray, 1.3, 5)
+#     faces = face_detector.detectMultiScale(img_gray, 1.3, 5)
 
-    count = 0
+#     count = 0
 
-    for (x, y, w, h) in faces:
-        img_face = cv2.resize(img[y: y + h, x: x + w], (64, 64))
+#     for (x, y, w, h) in faces:
+#         img_face = cv2.resize(img[y: y + h, x: x + w], (64, 64))
 
-        cv2.imwrite(img_path.replace('imgs', 'imgs_face').split('.jpg')[0] + '_{}.jpg'.format(count), img_face)
+#         cv2.imwrite(img_path.replace('imgs', 'imgs_face').split('.jpg')[0] + '_{}.jpg'.format(count), img_face)
 
-        count += 1
+#         count += 1
 
 
-image_path = 'imgs'
+# image_path = 'imgs'
 
-for whatelse in os.listdir(image_path):
+# for whatelse in os.listdir(image_path):
 
-    whatelse_path = os.path.join(image_path, whatelse)
+#     whatelse_path = os.path.join(image_path, whatelse)
 
-    for sub_whatelse in os.listdir(whatelse_path):
+#     for sub_whatelse in os.listdir(whatelse_path):
 
-        img_path = os.path.join(whatelse_path, sub_whatelse)
+#         img_path = os.path.join(whatelse_path, sub_whatelse)
 
-        if not os.path.isdir(whatelse_path.replace('imgs', 'imgs_face')):
+#         if not os.path.isdir(whatelse_path.replace('imgs', 'imgs_face')):
 
-            os.mkdir(whatelse_path.replace('imgs', 'imgs_face'))
+#             os.mkdir(whatelse_path.replace('imgs', 'imgs_face'))
 
-        if img_path.endswith('.jpg'):
-            getFaces(img_path)
+#         if img_path.endswith('.jpg'):
+#             getFaces(img_path)
+
+
